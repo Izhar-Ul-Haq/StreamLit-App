995f118f316f75e9392f34ed37af319fb25bbf0e
Izhar Ul Haq <iizharulhaq9@gmail.com>
2022-12-13 22:46:12 +0500

    first commit

---
+++ chatBOT.py
@@ -0,0 +1,10 @@
+# Import Libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+import plotly as pt
+import matplotlib.pyplot as plt
+# Created By Izhar Ul Haq#
+st.header("Hi! This is a gonna be a ChatterBot By Izhar")
+st.write("Hello World!!!")
+
